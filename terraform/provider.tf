78bb742ac6ca1ab7842efa6501fc6caf7efa8816
venkatsurya <venkatsurya1608@gmail.com>
2024-06-22 13:06:15 +0530

    some-message

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
     backend "s3" {    
         bucket = "venkatdevops-remote-state"    
-        key    = "terraform-expense-infra-frontend"  #everytime will change key   
+        key    = "terraform-expense-frontend-deploy"  #everytime will change key   
         region = "us-east-1" 
         dynamodb_table = "venkatdevops-locking" 
     }
